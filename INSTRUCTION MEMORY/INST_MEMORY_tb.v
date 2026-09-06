29c4ae18b7940c5ade59c63ae49b8ed871737c2f
het <hetrupala3710@gmail.com>
2026-09-06 10:52:22 +0530

    Refactor INST_MEMORY testbench
    Refactor testbench for INST_MEMORY to improve readability and structure.

--- INSTRUCTION MEMORY/INST_MEMORY_tb.v
+++ INSTRUCTION MEMORY/INST_MEMORY_tb.v
@@ -5,59 +5,53 @@ module INST_MEMORY_tb;
     reg [31:0] addr_in;
     wire [31:0] inst_out;
 
-    INST_MEMORY dut(
+    integer i;
+
+    INST_MEMORY uut (
         .addr_in(addr_in),
         .inst_out(inst_out)
     );
 
+    // ============================================================
+    // TEST
+    // ============================================================
+
     initial begin
 
-        // ADDI x1, x0, 10
-        addr_in = 32'h00000000;
-        #10;
+        #1;
 
-        // ADDI x2, x0, 20
-        addr_in = 32'h00000004;
-        #10;
+        $display("==================================================");
+        $display("INST_MEMORY TEST");
+        $display("==================================================");
 
-        // ADD x3, x1, x2
-        addr_in = 32'h00000008;
-        #10;
+        for(i = 0; i < 72; i = i + 1) begin
 
-        // SUB x4, x2, x1
-        addr_in = 32'h0000000C;
-        #10;
+            addr_in = i * 4;
 
-        // AND x5, x1, x2
-        addr_in = 32'h00000010;
-        #10;
+            #1;
 
-        // OR x6, x1, x2
-        addr_in = 32'h00000014;
-        #10;
+            $display("Address = %h    Instruction = %h",
+                     addr_in,
+                     inst_out);
 
-        // XOR x7, x1, x2
-        addr_in = 32'h00000018;
-        #10;
+        end
 
-        // SLT x8, x1, x2
-        addr_in = 32'h0000001C;
-        #10;
+        // ========================================================
+        // DEFAULT ADDRESS TEST
+        // ========================================================
 
-        // SLTU x9, x1, x2
-        addr_in = 32'h00000020;
-        #10;
+        addr_in = 32'h00000120;
 
-        // SLL x10, x1, x2
-        addr_in = 32'h00000024;
-        #10;
+        #1;
 
-        // SRL x11, x2, x1
-        addr_in = 32'h00000028;
-        #10;
+        $display("Address = %h    Instruction = %h",
+                 addr_in,
+                 inst_out);
 
-        // SRA x12, x2, x1
-        addr_in = 32'h0000002C;
+        $display("==================================================");
+        $display("INST_MEMORY TEST COMPLETE");
+        $display("==================================================");
+
         #10;
 
         $finish;
